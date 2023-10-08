efa901104bc35ace1642c27312cd1c8cacfb5339
Pranaya Deomani <pranayadeomani@proton.me>
2023-10-08 07:36:12 +0000

    hanoip: Define architecture configuration

--- device.mk
+++ device.mk
@@ -7,6 +7,16 @@
 # Inherit the proprietary files
 $(call inherit-product, vendor/motorola/hanoip/hanoip-vendor.mk)
 
+# Bionic
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.bionic.cpu_variant=cortex-a76 \
+    ro.bionic.2nd_cpu_variant=cortex-a55
+
+# Dalvik
+PRODUCT_PRODUCT_PROPERTIES += \
+    dalvik.vm.isa.arm64.variant=cortex-a76 \
+    dalvik.vm.isa.arm.variant=cortex-a55
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
