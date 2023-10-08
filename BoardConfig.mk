54d8bd8e2d6f5bf4f457ea9e2bf469978f4cbae9
Pranaya Deomani <pranayadeomani@proton.me>
2023-10-08 07:38:38 +0000

    hanoip: Add kernel configuration

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,5 +26,31 @@ TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 TARGET_BOARD_PLATFORM := sm6150
 TARGET_BOOTLOADER_BOARD_NAME := hanoip
 
+# Kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_CMDLINE := \
+    console=ttyMSM0,115200n8 \
+    androidboot.hardware=qcom \
+    androidboot.console=ttyMSM0 \
+    androidboot.memcg=1 \
+    lpm_levels.sleep_disabled=1 \
+    video=vfb:640x400,bpp=32,memsize=3072000 \
+    msm_rtb.filter=0x237 \
+    service_locator.enable=1 \
+    swiotlb=1 \
+    androidboot.usbcontroller=a600000.dwc3 \
+    earlycon=msm_geni_serial,0x880000 \
+    loop.max_part=7 \
+    printk.devkmsg=on \
+    firmware_class.path=/vendor/firmware_mnt/image \
+    androidboot.hab.csv=17 \
+    androidboot.hab.product=hanoip \
+    androidboot.hab.cid=50
+
+BOARD_BOOT_HEADER_VERSION := 3
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+
 # Inherit from the proprietary version
 include vendor/motorola/hanoip/BoardConfigVendor.mk
